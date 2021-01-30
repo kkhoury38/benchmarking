61a2bb9eaa6906799b35ff2948fb0734e2d2495b
Khalil Khoury <k.khoury@k-ren.gr>
2021-01-30 12:13:40 +0200

    raspberry sipo register benckmark

--- raspberry-sipo-register/Signal-Generator.vhdl
+++ raspberry-sipo-register/Signal-Generator.vhdl
@@ -1,16 +1,52 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL; 
+  
+entity clock_divider is
+  port( 
+    clk,reset: in std_logic;
+    clock_out: out std_logic
+  );
+end clock_divider; 
+
+architecture behavioral of clock_divider is
+  signal count: integer:=1;
+  signal tmp : std_logic := '0'; 
+begin
+  process(clk,reset)
+  begin
+    if(reset='1') then
+      count<=1;
+      tmp<='0';
+    elsif rising_edge(clk) then
+      count <=count+1;
+      -- 100MHz -> 2KHz: count = 25000
+	  -- 100MHz -> ~3.4 KHz: count = 14700
+	  --  12MHz -> 2KHz: count =  3000
+      if (count = 1000) then
+        tmp <= NOT tmp;
+        count <= 1;
+      end if;
+    end if;
+    
+    clock_out <= tmp;
+  end process;
+  
+end behavioral;
+
 ----
 library ieee; 
 use ieee.std_logic_1164.all; 
 use ieee.std_logic_unsigned.all;
 
-entity counter_input is
+entity signal_generator is
   port(
     clk: in std_logic;
     DATA, LOAD, CLK_OUTPUT: out std_logic 
   );
-end counter_input;
+end signal_generator;
  
-architecture behavioral of counter_input is 
+architecture behavioral of signal_generator is 
 signal temp_LOAD: std_logic;
 begin
   process (clk)
@@ -41,95 +77,38 @@ begin
   
 end behavioral;
 
--- Testbench for OR gate
-library IEEE;
-use IEEE.std_logic_1164.all;
+----
+library ieee;
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.ALL; 
+
+entity top_level is
+  port(
+    board_clock: in std_logic;
+	DATA_top, LOAD_top, CLK_OUTPUT_top, led8: out std_logic 
+  );
+end top_level;
  
-entity testbench is
--- empty
-end testbench; 
+architecture behavioral of top_level is
 
-architecture tb of testbench is
-
--- DUT component
-component counter_input is
- port(
+  component clock_divider
+  port( 
+    clk,reset: in std_logic;
+    clock_out: out std_logic
+  );
+  end component;
+  
+  component signal_generator is
+  port(
     clk: in std_logic;
     DATA, LOAD, CLK_OUTPUT: out std_logic 
   );
-end component;
-
-signal clk_in, DATA_out, LOAD_out, CLK_OUTPUT_out: std_logic;
-
+  end component;
+  
+  signal clk_divided, DATA_Temp: std_logic;
 begin
-
-  -- Connect DUT
-  DUT: counter_input port map(clk_in, DATA_out, LOAD_out, CLK_OUTPUT_out);
-
-  process
-  begin
-    clk_in <= '0';
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    clk_in <= NOT clk_in ;
-    wait for 1 ns;
-    
-    wait;
-  end process;
-end tb;
+  clk_div1: clock_divider
+    port map(clk => board_clock, reset => '0', clock_out => clk_divided);
+	
+  sig_gen: signal_generator
+    port map(clk => clk_divided, DATA => DATA_Temp, LOAD => LOAD_top, CLK_OUTPUT => CLK_OUTPUT_top);
